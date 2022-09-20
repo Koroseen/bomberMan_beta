a082fc5294cdc211710c084d44e71daaadb34c03
Hieu Ngo <hieungo1410@gmail.com>
2022-09-20 19:51:42 +0700

    little fixing

--- src/uet/oop/bomberman/entities/blocks/Brick.java
+++ src/uet/oop/bomberman/entities/blocks/Brick.java
@@ -2,10 +2,7 @@ package uet.oop.bomberman.entities.blocks;
 
 import javafx.scene.image.Image;
 import uet.oop.bomberman.entities.Entity;
-import uet.oop.bomberman.entities.EntityList;
 import uet.oop.bomberman.graphics.Sprite;
-
-import static uet.oop.bomberman.entities.EntityList.bomberman;
 
 
 public class Brick extends Entity {
--- src/uet/oop/bomberman/entities/blocks/Flame.java
+++ src/uet/oop/bomberman/entities/blocks/Flame.java
@@ -2,8 +2,6 @@ package uet.oop.bomberman.entities.blocks;
 
 import javafx.scene.image.Image;
 import uet.oop.bomberman.entities.Entity;
-import uet.oop.bomberman.entities.EntityList;
-import uet.oop.bomberman.graphics.Sprite;
 
 public class Flame extends Entity {
     public Flame(int xUnit, int yUnit, Image img) {
